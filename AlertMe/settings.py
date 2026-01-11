d2aa7effd1030a13f41ebf368417cb182f5f6859
Dasakami <dendasakami@gmail.com>
2026-01-11 17:50:55 +0600

    return ro render

--- AlertMe/settings.py
+++ AlertMe/settings.py
@@ -88,7 +88,7 @@ CACHES = {
 # ✅ Email настройки
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='kilin6786@gmail.com')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='yomg xwfw xbtm wshy')
